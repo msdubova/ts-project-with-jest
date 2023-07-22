afe957216489f46b394d8d775dea291b1f00564f
Your GitHub Usernamemsdubova <femme777@gmail.com>
2023-07-22 19:10:07 +0300

    TS environment set-up

---
+++ out/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const calc_1 = require("./calc");
+console.log((0, calc_1.add)(2, 2));
+//# sourceMappingURL=index.js.map
